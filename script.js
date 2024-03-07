2d881c73423fe96a2c7366e3ae7739b1fb4b529d
Will160714 <95445527+Will160714@users.noreply.github.com>
2024-03-07 16:56:26 -0500

    Removed the alerts for each function

--- script.js
+++ script.js
@@ -12,17 +12,14 @@ function addR() {
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
 }
 
 // Remove a row
 function removeR() {
-    alert("Clicked Remove Row"); // Replace this line with your code.
 }
 
 // Remove a column
 function removeC() {
-    alert("Clicked Remove Col"); // Replace this line with your code.
 }
 
 // Set global variable for selected color
@@ -33,15 +30,12 @@ function selectColor(){
 
 // Fill all uncolored cells
 function fillU(){
-    alert("Clicked Fill All Uncolored"); // Replace this line with your code.
 }
 
 // Fill all cells
 function fillAll(){
-    alert("Clicked Fill All"); // Replace this line with your code.
 }
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
 }
